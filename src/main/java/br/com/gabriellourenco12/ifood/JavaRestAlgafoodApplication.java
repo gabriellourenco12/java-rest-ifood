4090814ecffd3cb523a674488084351c1c1efd5d
gabriellourenco12 <gabriel9lourenco@gmail.com>
2023-07-29 16:44:51 -0300

    commit inicial

---
+++ src/main/java/br/com/gabriellourenco12/ifood/JavaRestAlgafoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriellourenco12.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestAlgafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestAlgafoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriellourenco12/ifood/JavaRestAlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriellourenco12.ifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestAlgafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
